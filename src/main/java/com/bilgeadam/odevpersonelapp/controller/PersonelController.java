495da1e63bae0edb9f437dcd2891146d0ddba413
hasan huseyin <hkizilay65@gmail.com>
2022-02-21 21:31:34 +0300

    personel bolumu yazildi.

---
+++ src/main/java/com/bilgeadam/odevpersonelapp/controller/PersonelController.java
@@ -0,0 +1,101 @@
+package com.bilgeadam.odevpersonelapp.controller;
+
+import com.bilgeadam.odevpersonelapp.entity.Personel;
+import com.bilgeadam.odevpersonelapp.exception.PersonelNotFoundException;
+import com.bilgeadam.odevpersonelapp.pojo.Bolum;
+import com.bilgeadam.odevpersonelapp.pojo.Sehir;
+import com.bilgeadam.odevpersonelapp.repository.PersonelRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+
+
+@RestController
+public class PersonelController {
+
+    @Autowired
+    private PersonelRepository personelRepository;
+
+    @Autowired
+    private EurekaClient client;
+
+    @GetMapping("/personel/{id}")
+    public Personel getPersonel(@PathVariable("id") long id)
+    {
+        Personel personel = null;
+        Optional<Personel> personelDB =personelRepository.findById(id);
+
+        if(personelDB.isPresent())
+        {
+            personel = personelDB.get();
+            return personel;
+        }
+        else
+        {
+            throw new PersonelNotFoundException(id + "nolu personel bulunamadi!");
+        }
+    }
+    @GetMapping("/personel")
+    public List<Personel> getTumPersonel()
+    {
+        return personelRepository.findAll();
+    }
+
+    @GetMapping("/personel-tam/{id}")
+    public String getPersonelTam(@PathVariable("id") long id)
+    {
+        Personel personel = getPersonel(id);
+
+        Bolum bolum = getBolum(personel.getBolumNo());
+
+        Sehir sehir = getSehir(bolum.getSehirNo());
+
+        return personel.getAd()+ " " +personel.getSoyad()+" "+bolum.getAd()+" "+sehir.getAd();
+
+
+    }
+
+    @GetMapping("/personel/bolum/{bolumNo}")
+    public List<Personel> getPersonelByBolumNo(long bolumNo)
+    {
+        return personelRepository.findPersonelByBolumNo(bolumNo);
+    }
+
+
+    private Bolum getBolum(long bolumNo)
+    {
+        //String bolumURL = "http://localhost:8230";
+
+        RestTemplate restTemplate = new RestTemplate();
+        InstanceInfo instanceInfo =
+                client.getNextServerFromEureka("odev-bolum-app",false);
+
+        String bolumURL = instanceInfo.getHomePageUrl();
+        Bolum bolumAd =  restTemplate.getForObject(bolumURL+"/bolum/"+bolumNo,Bolum.class);
+
+        return bolumAd;
+
+    }
+
+    private Sehir getSehir(long SehirNo)
+    {
+       // String SehirURL = "http://localhost:8240";
+
+        RestTemplate restTemplate = new RestTemplate();
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("odev-sehir-app",false);
+        String SehirURL = instanceInfo.getHomePageUrl();
+
+        Sehir Sehir =  restTemplate.getForObject(SehirURL+"/sehir/"+SehirNo,Sehir.class);
+
+        return Sehir;
+
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into personel(ad,soyad, bolum_no) values('Aziz','Sancar',1);
+insert into personel(ad,soyad, bolum_no) values('Ugur','Sahin',1);
+insert into personel(ad,soyad, bolum_no) values('Tanju','Colak',2);
